5e7b34fa5976a2a9cafb816d2cd99481de3ad8d8
Sneha Kumari <snehak.ug20.ee@nitp.ac.in>
2021-11-26 17:30:19 +0530

    initial commit

---
+++ Person.js
@@ -0,0 +1,15 @@
+class Person{
+    constructor(first_name, last_name ,address) {
+        this.firstName = first_name;
+        this.lastName = last_name;
+        this.officeAddress=address;
+    }
+}
+
+const p_one = new Person('Sneha', 'Kumari','Nawada, Bihar');
+const p_two = new Person('Ritiki', 'Pandey','Gorakhpur, UP');
+const p_three = new Person('Kajal', 'Kumari','Madhubani, Bihar');
+
+console.log(p_one);
+console.log(p_two);
+console.log(p_three);
